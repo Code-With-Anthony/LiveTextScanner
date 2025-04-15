a94973b376836d8e734d3adf5171eaaadce0b7ec
Anthony Dourado <anthony.dourado@thegatewaycorp.co.in>
2025-04-15 13:54:33 +0530

    bug fixing

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -4,7 +4,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/cards";
+} from "@/components/ui/card";
 import { Camera, FileText, Languages, Zap, Cloud, Lock } from "lucide-react";
 
 const Features = () => {
